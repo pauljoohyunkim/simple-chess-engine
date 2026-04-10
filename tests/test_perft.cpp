1936eb821bb2e6b725bf69e41a710ec97222b3e4
pauljoohyunkim <pauljoohyunkim@gmail.com>
2026-04-10 12:12:48 +0900

    Kiwipete and Position 4

--- tests/test_perft.cpp
+++ tests/test_perft.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "../include/chess.h"
 #include "../include/dev.h"
+#include "../include/fen.h"
 #include "setup.h"
 
 TEST(PERFT, Initial_Depth_1_to_5) {
@@ -16,6 +17,34 @@ TEST(PERFT, Initial_Depth_1_to_5) {
 }
 
 
-//TEST(PERFT, Kiwipete_Depth_1_to_5) {
-//
-//}
+TEST(PERFT, Kiwipete_Depth_1_to_5) {
+    const uint testvector[5U] = { 48, 2039, 97862, 4085603, 193690690 };
+    for (uint depth = 1U; depth <= 5U; depth++) {
+        SCE_Chessboard board;
+        ASSERT_EQ(SCE_Chessboard_FEN_setup(&board, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0"), SCE_SUCCESS);
+        SCE_PieceMovementPrecomputationTable precpt_tbl;
+        SCE_PieceMovementPrecompute(&precpt_tbl);
+
+        debug_print_board(&board);
+
+        const uint count = perft_count(&board, &precpt_tbl, depth);
+
+        ASSERT_EQ(count, testvector[depth-1]);
+    }
+}
+
+TEST(PERFT, TEST3) {
+    const uint testvector[4U] = { 14, 191, 2812, 43238 };
+    for (uint depth = 1U; depth <= 4U; depth++) {
+        SCE_Chessboard board;
+        ASSERT_EQ(SCE_Chessboard_FEN_setup(&board, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1"), SCE_SUCCESS);
+        SCE_PieceMovementPrecomputationTable precpt_tbl;
+        SCE_PieceMovementPrecompute(&precpt_tbl);
+
+        debug_print_board(&board);
+
+        const uint count = perft_count(&board, &precpt_tbl, depth);
+
+        ASSERT_EQ(count, testvector[depth-1]);
+    }
+}
