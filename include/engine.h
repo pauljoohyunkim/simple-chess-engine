e367f9c26b9943b6ab0bbcafec1aad822fbbff86
pauljoohyunkim <pauljoohyunkim@gmail.com>
2026-04-16 16:41:42 +0900

    Evaluation function wrapper

--- include/engine.h
+++ include/engine.h
@@ -14,6 +14,17 @@ typedef struct {
 
 } SCE_Engine;
 
+/**
+ * @brief Evaluate the chessboard from White's perspective
+ * 
+ * @param ptr_engine Pointer to the SCE_Engine struct.
+ * @param ptr_board Pointer to the SCE_Chessboard struct.
+ * @return int Centipawn value where positive means advantage for white and negative means advantage for black.
+ * 
+ * Note that this function does not return success or failure code.
+ */
+int SCE_Engine_EvaluateBoard(const SCE_Engine* const ptr_engine, const SCE_Chessboard* const ptr_board);
+
 #ifdef __cplusplus
 }
 #endif
--- src/engine.c
+++ src/engine.c
@@ -1,3 +1,7 @@
 #include "engine.h"
 
+int SCE_Engine_EvaluateBoard(const SCE_Engine* const ptr_engine, const SCE_Chessboard* const ptr_board) {
+    const int centipawn = ptr_engine->eval_function(ptr_board);
 
+    return centipawn;
+}
