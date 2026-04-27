9d53657677ccc8d6d9b2927b61892b536b66499f
pauljoohyunkim <pauljoohyunkim@gmail.com>
2026-04-27 15:52:39 +0900

    UCI commands

--- src/bin/sce_engine.c
+++ src/bin/sce_engine.c
@@ -1,6 +1,22 @@
+#include <stdio.h>
+#include <string.h>
 #include "chess.h"
 #include "uci.h"
 
 int main(int argc, char** argv) {
-
+    char line[BUFSIZ] = { 0 };
+    while (fgets(line, sizeof(line)-1, stdin)) {
+        if (strncmp(line, "uci", 3) == 0) {
+            printf("id name SimpleChessEngine\n");
+            printf("id author Paul Joo-Hyun Kim\n");
+            printf("uciok\n");
+        }
+        if (strncmp(line, "isready", 7) == 0) {
+            printf("readyok\n");
+        }
+        if (strncmp(line, "quit", 4) == 0) {
+            break;
+        }
+        fflush(stdout);
+    }
 }
