cbd3247cfac3190bf6a8d2fc0b27ad17af6c04eb
pauljoohyunkim <pauljoohyunkim@gmail.com>
2026-05-02 11:07:38 +0900

    Disable buffering

--- src/bin/sce_uci_engine.c
+++ src/bin/sce_uci_engine.c
@@ -10,6 +10,7 @@
 #define TT_TABLE_LOG_2_SIZE 24
 
 int main(int argc, char** argv) {
+    setvbuf(stdout, NULL, _IONBF, 0);
     SCE_Context ctx;
 
     SCE_Precomputation_Tables precomputation_tables;
@@ -19,6 +20,7 @@ int main(int argc, char** argv) {
     #else
     SCE_Return ret = SCE_Precomputation_Tables_init(&precomputation_tables, NULL);
     #endif
+    assert(ret == SCE_SUCCESS);
 
     ret = SCE_Context_init(&ctx, &precomputation_tables);
     assert(ret == SCE_SUCCESS);
@@ -37,38 +39,41 @@ int main(int argc, char** argv) {
 
     char line[BUFSIZ] = { 0 };
     while (fgets(line, sizeof(line)-1, stdin)) {
-        if (strncmp(line, "uci", 3) == 0) {
+        if (strncmp(line, "ucinewgame", 10) == 0) {
+            ret = SCE_Engine_release(&engine);
+            assert(ret == SCE_SUCCESS);
+            ret = SCE_Context_init(&ctx, &precomputation_tables);
+            assert(ret == SCE_SUCCESS);
+            ret = SCE_Engine_init(&ctx, &engine, SCE_Eval_SimplifiedEvaluationFunction, SCE_DeltaEval_SimplifiedEvaluationFunction, TT_TABLE_LOG_2_SIZE);
+            assert(ret == SCE_SUCCESS);
+        } else if (strncmp(line, "uci", 3) == 0) {
             pthread_mutex_lock(&session.stdout_mutex);
             printf("id name SimpleChessEngine\n");
             printf("id author Paul Joo-Hyun Kim\n");
             printf("uciok\n");
             pthread_mutex_unlock(&session.stdout_mutex);
             continue;
-        }
-        if (strncmp(line, "isready", 7) == 0) {
+        } else if (strncmp(line, "isready", 7) == 0) {
             pthread_mutex_lock(&session.stdout_mutex);
             printf("readyok\n");
             pthread_mutex_unlock(&session.stdout_mutex);
             continue;
-        }
-        if (strncmp(line, "position", 8) == 0) {
-            SCE_UCI_ParsePosition(&ctx, line);
+        } else if (strncmp(line, "position", 8) == 0) {
+            ret = SCE_UCI_ParsePosition(&ctx, line);
             continue;
-        }
-        if (strncmp(line, "go", 2) == 0) {
-            SCE_UCI_ParseGo(&session, line);
+        } else if (strncmp(line, "go", 2) == 0) {
+            ret = SCE_UCI_ParseGo(&session, line);
             continue;
-        }
-        if (strncmp(line, "print", 5) == 0) {
+        } else if (strncmp(line, "print", 5) == 0) {
             pthread_mutex_lock(&session.stdout_mutex);
-            SCE_Chessboard_print(&ctx, WHITE);
+            ret = SCE_Chessboard_print(&ctx, WHITE);
+            assert(ret = SCE_SUCCESS);
             pthread_mutex_unlock(&session.stdout_mutex);
             continue;
-        }
-        if (strncmp(line, "quit", 4) == 0) {
+        } else if (strncmp(line, "quit", 4) == 0) {
             break;
         }
-        fflush(stdout);
+        //fflush(stdout);
     }
 
     SCE_Engine_release(&engine);
