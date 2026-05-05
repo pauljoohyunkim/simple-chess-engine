fbbb50d5b25737d12f772e73144433cd544dbc12
Paul Kim <pauljoohyunkim@gmail.com>
2026-05-05 18:40:37 +0900

    HCEF Delta compute relevant info from move

--- src/eval/hcef.c
+++ src/eval/hcef.c
@@ -235,9 +235,161 @@ int SCE_Eval_HandcraftedEvaluationFunction(SCE_Context* const ctx, SCE_Engine* c
     return (mg_score * phase + eg_score * (TOTAL_PHASE_WEIGHT - phase)) / TOTAL_PHASE_WEIGHT;
 }
 
+#define IS_WHITE(piecetype) (piecetype < B_PAWN)
+#define IS_BLACK(piecetype) (piecetype >= B_PAWN)
 int SCE_DeltaEval_HandcraftedEvaluationFunction(SCE_Context* const ctx, SCE_EvalState* const ptr_eval_state, SCE_Engine* const ptr_engine, const SCE_ChessMove move) {
     assert(ctx != NULL);
     assert(ptr_eval_state != NULL);
+    assert(ptr_engine != NULL);
+    assert(move != EMPTY_MOVE);
+
+    uint64_t w_pawns = ctx->board.bitboards[W_PAWN];
+    uint64_t b_pawns = ctx->board.bitboards[B_PAWN];
+    uint64_t occupancy_w = SCE_Chessboard_Occupancy_Color(ctx, WHITE);
+    uint64_t occupancy_b = SCE_Chessboard_Occupancy_Color(ctx, BLACK);
+    uint64_t pawn_zobrist_hash = ctx->board.pawn_zobrist_hash;
+
+    // TODO: Modify occupancy_w, occupancy_b, pawn_zobrist_hash, w_pawns, b_pawns depending on the move.
+    // 0. No pawn involved general moves.
+    // 1. Move is a promotion/promo-capture.
+    // 2. Move is a pawn move (normal pawn move, pawn capture, en passant)
+    // 3. Move is capture not involving pawns
+    // 4. Move is castling.
+    // Use the implementation of MakeMove as a guide.
+    {
+        const uint src_idx = move SCE_CHESSMOVE_GET_SRC;
+        const uint dst_idx = move SCE_CHESSMOVE_GET_DST;
+        const uint64_t src = 1ULL << src_idx;
+        const uint64_t dst = 1ULL << dst_idx;
+        int captured_idx = UNASSIGNED;
+        const int flag = move SCE_CHESSMOVE_GET_FLAG;
+        const PieceType src_piece_type = ctx->board.mailbox[src_idx];
+        PieceType captured_piece_type = UNASSIGNED_PIECE_TYPE;
+        assert(src_piece_type != UNASSIGNED_PIECE_TYPE);
+
+        // Determine captured piece type.
+        if (flag == SCE_CHESSMOVE_FLAG_EN_PASSANT_CAPTURE) {
+            captured_idx = ctx->board.en_passant_idx;
+            captured_piece_type = ctx->board.mailbox[captured_idx];
+        } else {
+            captured_idx = dst_idx;
+            captured_piece_type = ctx->board.mailbox[dst_idx];
+        }
+
+        // Execution
+        // 1. Capture
+        // 2. Move
+        // 3. Flag action
+
+        // 1. Capture
+        if (flag & SCE_CHESSMOVE_FLAG_CAPTURE) {
+            assert(captured_piece_type != UNASSIGNED_PIECE_TYPE);
+            const uint64_t captured_piece = 1ULL << captured_idx;
+            if (flag == SCE_CHESSMOVE_FLAG_EN_PASSANT_CAPTURE) {
+                // En passant
+                assert(captured_piece_type == W_PAWN || captured_piece_type == B_PAWN);
+                if (captured_piece_type == W_PAWN) {
+                    w_pawns ^= captured_piece;
+                    occupancy_w ^= captured_piece;
+                    pawn_zobrist_hash ^= ctx->precomputation_tables->zobrist_table.piece_key[captured_piece_type][captured_idx];
+                } else {
+                    b_pawns ^= captured_piece;
+                    occupancy_b ^= captured_piece;
+                    pawn_zobrist_hash ^= ctx->precomputation_tables->zobrist_table.piece_key[captured_piece_type][captured_idx];
+                }
+            } else {
+                // Pawn specific
+                switch (captured_piece_type) {
+                    case W_PAWN:
+                        w_pawns ^= captured_piece;
+                        pawn_zobrist_hash ^= ctx->precomputation_tables->zobrist_table.piece_key[captured_piece_type][captured_idx];
+                        break;
+                    case B_PAWN:
+                        b_pawns ^= captured_piece;
+                        pawn_zobrist_hash ^= ctx->precomputation_tables->zobrist_table.piece_key[captured_piece_type][captured_idx];
+                        break;
+                    default:
+                        break;
+                }
+                if (IS_WHITE(captured_piece_type)) {
+                    // White captured
+                    occupancy_w ^= captured_piece;
+                } else {
+                    // Black captured
+                    occupancy_b ^= captured_piece;
+                }
+            }
+        }
+
+        // 2. Standard move
+        if (IS_WHITE(src_piece_type)) {
+            if (src_piece_type == W_PAWN) {
+                // Pawn specifics
+                w_pawns ^= src | dst;
+                pawn_zobrist_hash ^= ctx->precomputation_tables->zobrist_table.piece_key[src_piece_type][src_idx];
+                pawn_zobrist_hash ^= ctx->precomputation_tables->zobrist_table.piece_key[src_piece_type][dst_idx];
+            }
+            occupancy_w ^= src | dst;
+
+        } else {
+            // Black
+            if (src_piece_type == B_PAWN) {
+                // Pawn specifics
+                b_pawns ^= src | dst;
+                pawn_zobrist_hash ^= ctx->precomputation_tables->zobrist_table.piece_key[src_piece_type][src_idx];
+                pawn_zobrist_hash ^= ctx->precomputation_tables->zobrist_table.piece_key[src_piece_type][dst_idx];
+            }
+            occupancy_b ^= src | dst;
+        }
+
+        // 3. Flag
+        switch (flag) {
+            case SCE_CHESSMOVE_FLAG_KNIGHT_PROMOTION:
+            case SCE_CHESSMOVE_FLAG_KNIGHT_PROMO_CAPTURE:
+            case SCE_CHESSMOVE_FLAG_BISHOP_PROMOTION:
+            case SCE_CHESSMOVE_FLAG_BISHOP_PROMO_CAPTURE:
+            case SCE_CHESSMOVE_FLAG_ROOK_PROMOTION:
+            case SCE_CHESSMOVE_FLAG_ROOK_PROMO_CAPTURE:
+            case SCE_CHESSMOVE_FLAG_QUEEN_PROMOTION:
+            case SCE_CHESSMOVE_FLAG_QUEEN_PROMO_CAPTURE:
+                pawn_zobrist_hash ^= ctx->precomputation_tables->zobrist_table.piece_key[src_piece_type][dst_idx];
+                
+                if (IS_WHITE(src_piece_type)) {
+                    w_pawns ^= dst;
+                } else {
+                    b_pawns ^= dst;
+                }
+                break;
+            case SCE_CHESSMOVE_FLAG_KING_CASTLE:
+                {
+                    const uint rook_idx_src = dst_idx + 1U;
+                    const uint rook_idx_dst = dst_idx - 1U;
+                    const uint64_t rook_src = 1ULL << rook_idx_src;
+                    const uint64_t rook_dst = 1ULL << rook_idx_dst;
+                    if (IS_WHITE(src_piece_type)) {
+                        occupancy_w ^= rook_src ^ rook_dst;
+                    } else {
+                        occupancy_b ^= rook_src ^ rook_dst;
+                    }
+                }
+                break;
+            case SCE_CHESSMOVE_FLAG_QUEEN_CASTLE:
+                {
+                    const uint rook_idx_src = dst_idx - 2U;
+                    const uint rook_idx_dst = dst_idx + 1U;
+                    const uint64_t rook_src = 1ULL << rook_idx_src;
+                    const uint64_t rook_dst = 1ULL << rook_idx_dst;
+                    if (IS_WHITE(src_piece_type)) {
+                        occupancy_w ^= rook_src ^ rook_dst;
+                    } else {
+                        occupancy_b ^= rook_src ^ rook_dst;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
 
     int delta_score = SCE_DeltaEval_SimplifiedEvaluationFunction(ctx, ptr_eval_state, ptr_engine, move);
 
